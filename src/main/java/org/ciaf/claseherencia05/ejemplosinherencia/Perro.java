3cad6129b2115f43070c0581806dfa189aaac8f6
SantyOkami23 <santiago.ramirez5@utp.edu.co>
2024-09-03 21:13:16 -0500

    clase herencia

---
+++ src/main/java/org/ciaf/claseherencia05/ejemplosinherencia/Perro.java
@@ -0,0 +1,16 @@
+package org.ciaf.claseherencia05.ejemplosinherencia;
+
+// Clase Perro
+public class Perro {
+    String nombre;
+
+    // Constructor
+    public Perro(String nombre) {
+        this.nombre = nombre;
+    }
+
+    // Método para hacer ladrar al perro
+    public void ladrar() {
+        System.out.println("Guau guau!");
+    }
+}
