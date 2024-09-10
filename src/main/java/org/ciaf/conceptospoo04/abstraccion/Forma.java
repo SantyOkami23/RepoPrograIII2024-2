56444065659648eb3af7b601a4a2971776bdff2f
SantyOkami23 <santiago.ramirez5@utp.edu.co>
2024-09-10 17:23:47 -0500

    Se agregan ejemplos de herencia

--- src/main/java/org/ciaf/conceptospoo04/abstraccion/Forma.java
+++
@@ -1,8 +0,0 @@
-package org.ciaf.conceptospoo04.abstraccion;
-
-// Clase abstracta Forma
-abstract class Forma {
-    // Métodos abstractos
-    abstract double calcularArea();
-    abstract double calcularPerimetro();
-}
